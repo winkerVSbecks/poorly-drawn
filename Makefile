59fc3764c03b99f2a53897f64900bd528e04f198
winkerVSbecks <varunvachhar@gmail.com>
2021-02-14 20:59:44 -0500

    prepare for deploy

--- Makefile
+++ Makefile
@@ -1,5 +1,2 @@
 install:
-	cd image-component && npm i && npm run build && rm -rf node_modules
-	cp _redirects image-component/storybook-static
-	cd functions/shout-out-image && npm i
-	cd functions/process-url && npm i
+	cd functions/image && npm i
--- functions/image/render.js
+++ functions/image/render.js
@@ -7,14 +7,6 @@ const defaultSettings = {
   canvas,
 };
 
-canvasSketch(sketch, settings).then(() => {
-  // Once sketch is loaded & rendered, stream a PNG with node-canvas
-  const out = fs.createWriteStream('output.png');
-  const stream = canvas.createPNGStream();
-  stream.pipe(out);
-  out.on('finish', () => console.log('Done rendering'));
-});
-
 module.exports = function render({ sketch, settings }) {
   return canvasSketch(sketch, { ...defaultSettings, ...settings }).then(() => {
     const buffer = canvas.toBuffer('image/png', {
